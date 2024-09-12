f4b5208f3876eb5bf9757ce6c57b126a725b39b8
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-09-12 17:03:53 +0545

    Update ProductForm.tsx

--- app/admin/products/_components/ProductForm.tsx
+++ app/admin/products/_components/ProductForm.tsx
@@ -1,5 +1,5 @@
-import ActionButton from "@/components/Button/ActionButton";
+// import ActionButton from "@/components/Button/ActionButton";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
+// import { Textarea } from "@/components/ui/textarea";
