e22cfa8b5ef18be3feba75d2cc78413af769f0e8
RohitSth <shrestha.rohit655@gmail.com>
2024-09-16 15:30:26 +0545

    update :: added side hud component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,19 @@
 import Navbar from "@/app/_components/navbar";
+import { RightSideHudProvider } from "@/components/dom/RightSideHudProvider";
+import RightSideHud from '@/components/Huds/RightSideHud';
 
-export default async function MainLayout({ children }: { children: React.ReactNode }) {
+export default async function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
-      <Navbar />
-      <main className="mx-12 mt-12">{children}</main>
+      <RightSideHudProvider>
+        <RightSideHud />
+        <Navbar />
+        <main className="mx-12 mt-12">{children}</main>
+      </RightSideHudProvider>
     </>
   );
 }
