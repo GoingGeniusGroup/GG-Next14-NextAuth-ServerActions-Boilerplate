c684b2d8bc1aad6836023d600f85bdcba06aacc9
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-09-18 11:37:46 +0545

    Update CustomToolTipLeftRight.tsx

--- components/MyComponents/CustomToolTipLeftRight.tsx
+++ components/MyComponents/CustomToolTipLeftRight.tsx
@@ -1,6 +1,17 @@
-// NOTE: make sure you add group in the parent element className
-export default function CustomToolTipLeftRight({ content, top, left, translateY }) {
-  const isRightAligned = left < 0
+interface toolTip {
+  content: String;
+  top: String;
+  left: number;
+  translateY: String;
+}
+
+export default function CustomToolTipLeftRight({
+  content,
+  top,
+  left,
+  translateY,
+}: toolTip) {
+  const isRightAligned = left < 0;
 
   return (
     <>
@@ -10,16 +21,18 @@ export default function CustomToolTipLeftRight({ content, top, left, translateY 
           rounded-md bg-indigo-100 px-2 py-1
           text-sm text-indigo-800 opacity-20 transition-all
           group-hover:visible group-hover:opacity-100
-          ${isRightAligned ? 'right-0' : 'left-0'}
+          ${isRightAligned ? "right-0" : "left-0"}
         `}
         style={{
           top: `${top}px`,
-          ...(isRightAligned ? { right: `${Math.abs(left)}px` } : { left: `${left}px` }),
+          ...(isRightAligned
+            ? { right: `${Math.abs(left)}px` }
+            : { left: `${left}px` }),
           transform: `translateY(${translateY}px)`,
         }}
       >
         {content}
       </div>
     </>
-  )
+  );
 }
