16628d945a70b0aa265e35c97f6faf192a4c7791
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-09-19 14:35:28 +0545

    unused_import

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,21 +1,11 @@
-import React from "react";
-import { CheckCircle2 } from "lucide-react";
 
 
 import { Button } from "@/components/ui/button";
 // import PageHeader from "@/components/PageHeader/PageHeader";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../../../components/ui/dropdown-menu";
 import Link from "next/link";
 import ProductTable from "./_components/ProductTable";
-import ProductForm from "@/components/form/ProductForm";
+// import ProductForm from "@/components/form/ProductForm";
 // import ProductModel from "@/components/Model/ProductModel";
-import { db } from "@/lib/db";
 
 const page = () => {
   return (
