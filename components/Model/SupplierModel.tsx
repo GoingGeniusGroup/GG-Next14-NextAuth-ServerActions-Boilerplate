f040a778a7267585571e86388c2044de06557614
Sanjay Karki <karkisanjay2002@gmail.com>
2024-09-27 13:00:08 +0545

    update:: add model skeleton, Model for supplier and form

---
+++ components/Model/SupplierModel.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Modal from "@/components/ui/model";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Supplier } from "@prisma/client";
+
+const SupplierModel = ({
+  edit = false,
+  supplier,
+}: {
+  edit?: boolean;
+  supplier?: Supplier;
+}) => {
+  const [openModel, setOpenModel] = useState(false);
+
+  return (
+    <>
+      <Modal open={openModel} onOpenChange={setOpenModel}>
+        {edit ? (
+          <Modal.Button asChild>
+            <div
+              onClick={() => setOpenModel(true)} // Ensure it toggles open state
+              className="cursor-pointer text-sm  
+               hover:bg-gray-100 p-2 rounded-md"
+            >
+              Edit
+            </div>
+          </Modal.Button>
+        ) : (
+          <Modal.Button>
+            <Button onClick={() => setOpenModel(true)}>Add Supplier</Button>
+          </Modal.Button>
+        )}
+
+        {openModel && (
+          <Modal.Content title={edit ? "Edit Supplier" : "Add Supplier"}>
+            <SupplierForm supplier={supplier ?? undefined} edit={edit} />
+          </Modal.Content>
+        )}
+      </Modal>
+    </>
+  );
+};
+
+export default SupplierModel;
