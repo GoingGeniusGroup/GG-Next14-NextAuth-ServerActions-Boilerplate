26af8329daa8d782255f80afec098e87bc9008d1
RohitSth <shrestha.rohit655@gmail.com>
2024-10-02 16:48:49 +0545

    update :: fixed the contents displaying in floating-dock

--- app/(main)/floating-dock/page.tsx
+++ app/(main)/floating-dock/page.tsx
@@ -1,10 +1,10 @@
-import { CardHoverEffectDemo } from "@/components/AceternityUI/CardHoverEffect";
+import { FloatingDockDemo } from "@/components/AceternityUI/FloatingDock";
 
 export default function FloatingDock() {
   return (
     <div className="bg-gradient-to-br h-screen p-8 from-black to-white">
       <div className="flex items-end justify-center size-full">
-        <CardHoverEffectDemo />;
+        <FloatingDockDemo />;
       </div>
     </div>
   );
