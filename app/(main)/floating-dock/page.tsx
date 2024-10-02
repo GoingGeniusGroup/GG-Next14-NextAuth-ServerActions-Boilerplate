5e51e3109efde9dce098b65406c1c9cddfd23d01
RohitSth <shrestha.rohit655@gmail.com>
2024-10-02 16:52:07 +0545

    update :: minor update in the floating dock page

--- app/(main)/floating-dock/page.tsx
+++ app/(main)/floating-dock/page.tsx
@@ -4,7 +4,7 @@ export default function FloatingDock() {
   return (
     <div className="bg-gradient-to-br h-screen p-8 from-black to-white">
       <div className="flex items-end justify-center size-full">
-        <FloatingDockDemo />;
+        <FloatingDockDemo />
       </div>
     </div>
   );
