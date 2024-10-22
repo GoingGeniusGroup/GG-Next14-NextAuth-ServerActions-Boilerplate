f5fba1978753c5f38f2977439cc63dc5d96486d0
RohitSth <shrestha.rohit655@gmail.com>
2024-10-22 10:30:34 +0545

    update :: merge the bento, lens and hover card effect

--- app/(main)/genius-profile/[username]/page.tsx
+++ app/(main)/genius-profile/[username]/page.tsx
@@ -1,14 +1,22 @@
 import AboutSection from "@/components/layouts/console/AboutSection";
 import BottomSection from "@/components/layouts/console/BottomSection";
+import CustomGalleryComponent from "@/components/layouts/gallery/CustomGalleryComponent";
 import GeniusUserAvatar from "@/components/layouts/GeniusUserProfile/GeniusUserAvatar";
 import GeniusUserHome from "@/components/layouts/GeniusUserProfile/GeniusUserHome";
 import GeniusUserProjectsComponent from "@/components/layouts/GeniusUserProfile/GeniusUserProjectsComponent";
 import BentoGridComponent from "@/components/layouts/grid/bento-grid-1";
 import { CollapsibleSidebarTabs } from "@/components/ui/tabs/custom-tabs";
 import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
   IconExposure,
+  IconFileBroken,
   IconHome,
   IconPhoto,
+  IconSignature,
+  IconTableColumn,
   IconTool,
 } from "@tabler/icons-react";
 import { ChevronRight } from "lucide-react";
@@ -30,6 +38,59 @@ type Tab = {
 export default function GeniusProfilePage({ params }: ProfileViewProps) {
   const username = params.username;
 
+  const images = [
+    {
+      title: "The Dawn of Innovation",
+      description: "Explore the birth of groundbreaking ideas and inventions.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Digital Revolution",
+      description: "Dive into the transformative power of technology.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Art of Design",
+      description: "Discover the beauty of thoughtful and functional design.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Power of Communication",
+      description:
+        "Understand the impact of effective communication in our lives.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Pursuit of Knowledge",
+      description: "Join the quest for understanding and enlightenment.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Joy of Creation",
+      description: "Experience the thrill of bringing ideas to life.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+      title: "The Spirit of Adventure",
+      description: "Embark on exciting journeys and thrilling discoveries.",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+    },
+  ];
+
   const tabs: Tab[] = [
     {
       title: "Home",
@@ -47,7 +108,7 @@ export default function GeniusProfilePage({ params }: ProfileViewProps) {
       icon: <IconPhoto size={24} />,
       content: (
         <div>
-          <DummyContent />
+          <CustomGalleryComponent items={images} />
         </div>
       ),
     },
@@ -81,15 +142,6 @@ export default function GeniusProfilePage({ params }: ProfileViewProps) {
   );
 }
 
-const DummyContent = () => {
-  return (
-    <Image
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQOmWaUAEICbwQ6IZFTNvszqzxitoRog0MQw&s"
-      alt="dummy image"
-      width="1000"
-      height="1000"
-      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
-      unoptimized
-    />
-  );
-};
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
---
+++ components/layouts/gallery/CustomGalleryComponent.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  BentoGridHover,
+  BentoGridHoverItem,
+} from "@/components/ui/bento-grid/bento-grid-hover";
+
+interface ItemsProp {
+  items: {
+    title: string;
+    description: string;
+    icon: React.ReactNode;
+    image: string;
+  }[];
+}
+
+export default function CustomGalleryComponent({ items }: ItemsProp) {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <BentoGridHover className="px-6">
+      {items.map((item, idx) => (
+        <BentoGridHoverItem
+          key={idx}
+          title={item.title}
+          description={item.description}
+          header={item.image}
+          icon={item.icon}
+          className={idx === 3 || idx === 6 ? "md:col-span-2" : ""}
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          isHovered={hoveredIndex === idx}
+        />
+      ))}
+    </BentoGridHover>
+  );
+}
