af03faf84f9cfbaca84b5e212feafeb75bfe898f
RohitSth <shrestha.rohit655@gmail.com>
2024-10-24 13:31:35 +0545

    update :: showed the current user info

--- components/comp/console/AboutSection.tsx
+++ components/comp/console/AboutSection.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import { getCurrentUser } from "@/actions/userAndGuild";
 import {
   Icon360View,
   IconActivityHeartbeat,
@@ -8,97 +7,70 @@ import {
   IconTrophy,
 } from "@tabler/icons-react";
 
-interface AboutSectionProps {
-  aboutUser: {
-    name: string;
-    faculty: string;
-    guild?: "BUDDHA" | "VAJRA" | "PADMA" | "RATNA" | "KARMA";
-    age?: number;
-    email: string;
-    username: string;
-    description: string;
-    updatedAt?: string;
-  };
-}
+export default async function AboutSection({ username }: { username: string }) {
+  const currentUser = await getCurrentUser();
 
-import { usePathname } from "next/navigation";
+  const owner = currentUser?.username === username;
 
-export default function AboutSection({ aboutUser }: AboutSectionProps) {
-  const pathname = usePathname();
   return (
     <div className="flex flex-col gap-y-16">
       <div className="flex flex-col gap-4 mb-3">
         <div className="flex items-center gap-3 text-black dark:text-gray-300">
-          <span className="uppercase">{aboutUser.name}</span> |{" "}
-          <span>
-            {aboutUser.updatedAt
-              ? aboutUser.updatedAt
-              : aboutUser.guild
-              ? aboutUser.guild
-              : "NO GUILD"}
+          <span className="uppercase">
+            {owner ? currentUser?.username : username}
           </span>{" "}
-          {aboutUser.age && <span>|</span>}
-          {aboutUser.age && <span>{aboutUser.age}</span>}
+          |{" "}
+          <span>
+            {owner ? currentUser?.created_at?.toString() : "SOME DATE"}
+          </span>{" "}
+          <span>|</span>
+          {owner ? (
+            <span>{currentUser?.dob?.toString()}</span>
+          ) : (
+            <span>19</span>
+          )}
         </div>
       </div>
-      {pathname.startsWith("/genius-profile") && (
-        <div className="gap-2 flex flex-wrap overflow-x-auto w-full">
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconMedal className="flex justify-center items-center size-full p-2 text-emerald-500" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconMedal2 className="flex justify-center items-center size-full p-2 text-amber-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconTrophy className="flex justify-center items-center size-full p-2 text-yellow-500" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
-          </div>
-          <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
-            <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
-          </div>
+      <div className="gap-2 flex flex-wrap overflow-x-auto w-full">
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconMedal className="flex justify-center items-center size-full p-2 text-emerald-500" />
         </div>
-      )}
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <Icon360View className="flex justify-center items-center size-full p-2 text-purple-600" />
+        </div>
+        <div className="size-12 bg-black/20 dark:bg-white/20 rounded-full text-black dark:text-white">
+          <IconActivityHeartbeat className="flex justify-center items-center size-full p-2 text-red-600" />
+        </div>
+      </div>
       <p
-        className={`text-black dark:text-gray-300 overflow-hidden text-ellipsis whitespace-normal line-clamp-2 ${
-          pathname.startsWith("/genius-profile") ? "" : "mt-20"
+        className={`text-black dark:text-gray-300 overflow-hidden text-ellipsis whitespace-normal line-clamp-2 mt-20
         }`}
       >
-        {aboutUser.description}
+        {owner ? currentUser?.email : "user email"}
       </p>
     </div>
   );
