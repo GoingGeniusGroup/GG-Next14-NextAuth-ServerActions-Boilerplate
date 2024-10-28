e855cdbd4cd502cbac23f7e5084a8baf2a3c2fdd
RohitSth <shrestha.rohit655@gmail.com>
2024-10-28 13:46:41 +0545

    update :: made the parallex work in the experience page

--- app/(main)/genius-profile-parallel/[username]/@experience/page.tsx
+++ app/(main)/genius-profile-parallel/[username]/@experience/page.tsx
@@ -2,8 +2,8 @@ import GeniusUserExperience from "@/components/comp/GeniusUserProfile/GeniusUser
 
 export default async function GeniusUserExperiencePage() {
   return (
-    <>
+    <div className="h-screen overflow-auto">
       <GeniusUserExperience />
-    </>
+    </div>
   );
 }
