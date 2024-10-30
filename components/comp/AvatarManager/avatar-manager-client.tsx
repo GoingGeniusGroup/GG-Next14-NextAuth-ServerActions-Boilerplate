7990444a06a8a855ddf7f5f425e7a156b984bc65
RohitSth <shrestha.rohit655@gmail.com>
2024-10-30 19:50:44 +0545

    update :: made the avatar uninteractable

--- components/comp/AvatarManager/avatar-manager-client.tsx
+++ components/comp/AvatarManager/avatar-manager-client.tsx
@@ -239,135 +239,146 @@ export default function AvatarManagerClient({
   };
 
   return (
-    <div className="space-x-4 flex justify-betweens w-full text-black dark:text-white">
-      <div className="w-1/2 relative backdrop-blur-lg rounded-lg border dark:border-white/20 border-black/20 hover:border-yellow-500 transition-all duration-300 ease-in-out">
-        <Card>
-          <CardHeader>
-            <CardTitle>Avatar Showcase</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="h-[500px] w-full relative">
-              <Avatar
-                modelSrc={
-                  selectedAvatar ||
-                  "https://models.readyplayer.me/658be9e8fc8bec93d06806f3.glb?morphTargets=ARKit,Eyes Extra&textureAtlas=none&lod=0"
-                }
-                shadows
-                animationSrc={currentEmote}
-                fov={40}
-                cameraTarget={1.5}
-                cameraInitialDistance={30}
-                effects={{
-                  ambientOcclusion: true,
-                }}
-              />
-            </div>
-          </CardContent>
-        </Card>
-        <div className="absolute top-4 right-4 z-40">
-          <Dialog
-            open={isAvatarCreatorOpen}
-            onOpenChange={setIsAvatarCreatorOpen}
-          >
-            <DialogTrigger asChild>
-              <MovingBorderButton onClick={handleCreateAvatar} className="p-2">
-                {isProcessing ? "..." : "Create New Avatar"}
-              </MovingBorderButton>
-            </DialogTrigger>
-            <DialogContent>
-              <div className="h-[600px] w-full relative rounded-xl overflow-hidden">
-                <AvatarCreator
-                  subdomain="gguser"
-                  config={
-                    editingAvatar
-                      ? {
-                          ...editAvatarConfig,
-                          avatarId: extractUserId(editingAvatar.avatar_url),
-                        }
-                      : createAvatarConfig
+    <>
+      <div className="flex w-full p-2 justify-center font-bold text-black dark:text-white">
+        AVATAR MANAGER
+      </div>
+      <div className="space-x-4 flex justify-betweens w-full text-black dark:text-white">
+        <div className="w-1/2 relative backdrop-blur-lg rounded-lg border dark:border-white/20 border-black/20 hover:border-yellow-500 transition-all duration-300 ease-in-out">
+          <Card>
+            <CardHeader>
+              <CardTitle>Avatar Showcase</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="h-[500px] w-full relative">
+                <Avatar
+                  modelSrc={
+                    selectedAvatar ||
+                    "https://models.readyplayer.me/658be9e8fc8bec93d06806f3.glb?morphTargets=ARKit,Eyes Extra&textureAtlas=none&lod=0"
                   }
-                  onAvatarExported={
-                    editingAvatar ? handleUpdateAvatar : handleAvatarCreated
-                  }
-                  onUserSet={(event) => console.log("User set:", event)}
-                  iframeUrl={
-                    editingAvatar
-                      ? getAvatarCreatorUrl(editingAvatar.avatar_url)
-                      : undefined
-                  }
+                  shadows
+                  animationSrc={currentEmote}
+                  style={{ pointerEvents: "none" }}
+                  fov={40}
+                  cameraTarget={1.5}
+                  cameraInitialDistance={30}
+                  effects={{
+                    ambientOcclusion: true,
+                  }}
                 />
-                {isProcessing && (
-                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    {editingAvatar
-                      ? "Updating avatar..."
-                      : "Creating avatar..."}
-                  </div>
-                )}
               </div>
-            </DialogContent>
-          </Dialog>
-        </div>
-      </div>
-
-      <div className="w-1/2 relative h-[700px] backdrop-blur-lg rounded-lg border dark:border-white/20 border-black/20 hover:border-yellow-500 transition-all duration-300 ease-in-out">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 h-[99%] overflow-auto">
-          {avatars.map((avatar) => (
-            <Card
-              key={avatar.avatar_id}
-              className={`border h-fit rounded-lg hover:border-yellow-500 transition-all duration-300 ease-in-out ${
-                selectedAvatar === avatar.avatar_url
-                  ? "border-sky-500"
-                  : "border-black/20 dark:border-white/20"
-              }`}
+            </CardContent>
+          </Card>
+          <div className="absolute top-4 right-4 z-40">
+            <Dialog
+              open={isAvatarCreatorOpen}
+              onOpenChange={setIsAvatarCreatorOpen}
             >
-              <CardContent className="relative pt-6">
-                <div className="flex flex-col items-center space-y-4">
-                  <Image
-                    src={
-                      avatar.avatar_url?.replace(".glb", ".png") ||
-                      "/placeholder-avatar.png"
+              <DialogTrigger asChild>
+                <MovingBorderButton
+                  onClick={handleCreateAvatar}
+                  className="p-2"
+                >
+                  {isProcessing ? "..." : "Create New Avatar"}
+                </MovingBorderButton>
+              </DialogTrigger>
+              <DialogContent>
+                <div className="h-[600px] w-full relative rounded-xl overflow-hidden">
+                  <AvatarCreator
+                    subdomain="gguser"
+                    config={
+                      editingAvatar
+                        ? {
+                            ...editAvatarConfig,
+                            avatarId: extractUserId(editingAvatar.avatar_url),
+                          }
+                        : createAvatarConfig
                     }
-                    alt="Avatar"
-                    width={128}
-                    height={128}
-                    className="rounded-full"
+                    onAvatarExported={
+                      editingAvatar ? handleUpdateAvatar : handleAvatarCreated
+                    }
+                    onUserSet={(event) => console.log("User set:", event)}
+                    iframeUrl={
+                      editingAvatar
+                        ? getAvatarCreatorUrl(editingAvatar.avatar_url)
+                        : undefined
+                    }
                   />
-                  <Button
-                    variant="black"
-                    size="sm"
-                    className={`hover:text-yellow-500 w-full ${
-                      selectedAvatar === avatar.avatar_url ? "text-sky-500" : ""
-                    }`}
-                    onClick={() => setSelectedAvatar(avatar.avatar_url)}
-                  >
-                    {selectedAvatar === avatar.avatar_url
-                      ? "Selected"
-                      : "Select"}
-                  </Button>
+                  {isProcessing && (
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                      {editingAvatar
+                        ? "Updating avatar..."
+                        : "Creating avatar..."}
+                    </div>
+                  )}
                 </div>
-                <div className="absolute top-2 flex gap-2 right-2">
-                  <Button
-                    variant="transparent_rounded"
-                    className="hover:text-yellow-500 text-sky-400 p-[1px]"
-                    size="mini2"
-                    onClick={() => handleEditAvatar(avatar)}
-                  >
-                    <IconEdit />
-                  </Button>
-                  <Button
-                    variant="transparent_rounded"
-                    className="hover:text-yellow-500 text-red-400 p-[1px]"
-                    size="mini2"
-                    onClick={() => handleDeleteAvatar(avatar.avatar_id)}
-                  >
-                    <IconTrash />
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+
+        <div className="w-1/2 relative h-[700px] backdrop-blur-lg rounded-lg border dark:border-white/20 border-black/20 hover:border-yellow-500 transition-all duration-300 ease-in-out">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 h-[99%] overflow-auto">
+            {avatars.map((avatar) => (
+              <Card
+                key={avatar.avatar_id}
+                className={`border h-fit rounded-lg hover:border-yellow-500 transition-all duration-300 ease-in-out ${
+                  selectedAvatar === avatar.avatar_url
+                    ? "border-sky-500"
+                    : "border-black/20 dark:border-white/20"
+                }`}
+              >
+                <CardContent className="relative pt-6">
+                  <div className="flex flex-col items-center space-y-4">
+                    <Image
+                      src={
+                        avatar.avatar_url?.replace(".glb", ".png") ||
+                        "/placeholder-avatar.png"
+                      }
+                      alt="Avatar"
+                      width={128}
+                      height={128}
+                      className="rounded-full"
+                    />
+                    <Button
+                      variant="black"
+                      size="sm"
+                      className={`hover:text-yellow-500 w-full ${
+                        selectedAvatar === avatar.avatar_url
+                          ? "text-sky-500"
+                          : ""
+                      }`}
+                      onClick={() => setSelectedAvatar(avatar.avatar_url)}
+                    >
+                      {selectedAvatar === avatar.avatar_url
+                        ? "Selected"
+                        : "Select"}
+                    </Button>
+                  </div>
+                  <div className="absolute top-2 flex gap-2 right-2">
+                    <Button
+                      variant="transparent_rounded"
+                      className="hover:text-yellow-500 text-sky-400 p-[1px]"
+                      size="mini2"
+                      onClick={() => handleEditAvatar(avatar)}
+                    >
+                      <IconEdit />
+                    </Button>
+                    <Button
+                      variant="transparent_rounded"
+                      className="hover:text-yellow-500 text-red-400 p-[1px]"
+                      size="mini2"
+                      onClick={() => handleDeleteAvatar(avatar.avatar_id)}
+                    >
+                      <IconTrash />
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
