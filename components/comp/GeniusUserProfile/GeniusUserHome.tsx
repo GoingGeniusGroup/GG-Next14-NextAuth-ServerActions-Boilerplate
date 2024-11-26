2397b63629b89b7df7234e3628a999a4161cc99b
RohitSth <shrestha.rohit655@gmail.com>
2024-11-26 14:16:29 +0545

    update :: updated the about section

--- components/comp/GeniusUserProfile/GeniusUserHome.tsx
+++ components/comp/GeniusUserProfile/GeniusUserHome.tsx
@@ -17,12 +17,12 @@ export default async function GeniusUserHome({
   return (
     <>
       <div className="flex w-full justify-between">
-        <div className="w-[35%] px-4">
+        <div className="w-[33%] px-4">
           <AboutSectionProfile
             userInfo={LoggedUserProfile ? currentUser : profileOwner}
           />
         </div>
-        <div className="w-[35%] px-4">
+        <div className="w-[33%] px-4">
           {/* Projects Grid */}
           <BottomSection
             userInfo={LoggedUserProfile ? currentUser : profileOwner}
--- components/comp/console/AboutSectionProfile.tsx
+++ components/comp/console/AboutSectionProfile.tsx
@@ -4,8 +4,6 @@ import {
   IconMedal,
 } from "@tabler/icons-react";
 import UpdateProfileDialog from "../Modal/profile/UpdateProfileDialog";
-import { calculateAge } from "@/utils/dateFormatter";
-import { IconCake } from "@tabler/icons-react";
 import Image from "next/image";
 
 export default async function AboutSectionProfile({
@@ -13,10 +11,6 @@ export default async function AboutSectionProfile({
 }: {
   userInfo: any;
 }) {
-  const userDob = userInfo?.dob;
-
-  const { age, formattedDob } = calculateAge(userDob ?? null);
-
   return (
     <>
       {/* Div with user information */}
@@ -60,82 +54,14 @@ export default async function AboutSectionProfile({
           </div>
           {/* top section */}
           <div className="relative w-full rounded-md bg-black/10 dark:bg-white/10 hover:dark:bg-white/20 hover:bg-black/20 transition-all duration-300 ease-in-out px-2 py-1 dark:text-white text-black">
-            <div className="flex w-full items-center justify-between">
-              <div className="text-[16px] font-semibold flex gap-x-1 items-center">
-                {userInfo ? (
-                  <p>
-                    {userInfo.first_name} {userInfo.last_name}
-                  </p>
-                ) : (
-                  <p>Firstname Lastname</p>
-                )}
-
-                {userInfo ? (
-                  <>
-                    {formattedDob && (
-                      <>
-                        <span className="text-sm font-semibold">•</span>
-                        {age && (
-                          <span className="text-sm font-semibold">{age}</span>
-                        )}
-                      </>
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <span className="text-sm font-semibold">•</span>
-                    <span className="text-sm font-semibold">AGE</span>
-                  </>
-                )}
-              </div>
-              {userInfo ? (
-                <div className="text-xs font-semibold flex items-center gap-1">
-                  {formattedDob && (
-                    <>
-                      <IconCake size={12} className="text-pink-500" />
-                      {formattedDob}
-                    </>
-                  )}
-                </div>
-              ) : (
-                <div className="text-xs font-semibold flex items-center gap-1">
-                  <IconCake size={12} className="text-pink-500" />
-                  DOB
-                </div>
-              )}
+            <div className="h-[60px] w-full overflow-auto text-[12px] font-semibold flex flex-col">
+              <span>Bio</span>
+              <span>{userInfo && userInfo.description}</span>
             </div>
-            <div className="h-[60px] w-full overflow-auto text-[12px] font-semibold">
-              <span>{userInfo ? userInfo.description : "BIO"}</span>
-            </div>
-          </div>
-          {/* bottom section */}
-          <div className="relative w-full rounded-md bg-black/10 dark:bg-white/10 px-2 py-1 hover:dark:bg-white/20 hover:bg-black/20 dark:text-white text-black transition-all duration-300 ease-in-out">
-            <p
-              className={`text-black dark:text-gray-300 flex justify-between items-center overflow-hidden text-ellipsis whitespace-normal line-clamp-2 font-semibold transition-all duration-500 ease-in-out`}
-            >
-              <span className="hover-black text-sm dark:hover:text-white cursor-pointer">
-                ADDRESS
-              </span>
-              <span className="text-xs hover-black dark:hover:text-white cursor-pointer">
-                {userInfo ? userInfo.address : "ADDRESS"}
-              </span>
-            </p>
-
-            <p
-              className={`text-black dark:text-gray-300 flex justify-between items-center overflow-hidden text-ellipsis whitespace-normal line-clamp-2 font-semibold transition-all duration-300 ease-in-out`}
-            >
-              <span className="hover-black text-sm dark:hover:text-white cursor-pointer">
-                JOINED
-              </span>
-              <span className="text-xs hover-black dark:hover:text-white cursor-pointer">
-                {userInfo?.created_at
-                  ? new Date(userInfo.created_at).toLocaleDateString()
-                  : "N/A"}
-              </span>
-            </p>
           </div>
         </div>
       </div>
+
       {/* Div with achievements */}
       <div className="relative flex flex-col gap-4 border mt-4 p-4 rounded-xl backdrop-blur-md border-black/10 dark:border-white/10 dark:hover:border-[#FCBB3F]/60 hover:border-sky-500/60 transition-all duration-200 ease-in-out">
         <div className="gap-2 flex flex-wrap overflow-x-auto w-full">
