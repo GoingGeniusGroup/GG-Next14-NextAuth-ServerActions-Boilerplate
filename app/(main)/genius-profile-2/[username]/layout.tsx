ad1606fa3c75efb726f4c35492178752390885f3
RohitSth <shrestha.rohit655@gmail.com>
2024-11-28 16:30:27 +0545

    update :: updated the coustom tooltip dessign

--- app/(main)/genius-profile-2/[username]/layout.tsx
+++ app/(main)/genius-profile-2/[username]/layout.tsx
@@ -10,6 +10,8 @@ import { Button } from "@/components/ui/button";
 import DragCloseDrawer from "@/components/comp/CustomComponents/DragCloseDrawer";
 
 import { LuGalleryVertical } from "react-icons/lu";
+import CustomToolTipLeftRight from "@/components/comp/CustomComponents/CustomToolTipLeftRight";
+import CustomToolTip from "@/components/comp/CustomComponents/CustomToolTip";
 interface GeniusProfileLayoutProps {
   userinfo: ReactNode;
   children: ReactNode;
@@ -59,21 +61,39 @@ export default function GeniusProfileLayout({
                 className={`absolute -bottom-2 z-40 left-1/2 -translate-x-1/2 flex space-x-4 mb-4 transition-all duration-300 ease-in-out`}
               >
                 <ul className="relative mx-auto flex w-fit gap-1 rounded-full bg-white/20 p-1">
-                  <Link href={`/genius-profile-2/${username}/gallery`}>
+                  <Link
+                    href={`/genius-profile-2/${username}/gallery`}
+                    className="group"
+                  >
                     <Button
                       className="rounded-full size-[32px] flex justify-center items-center bg-white text-black hover:bg-black hover:text-white transition-color duration-300 ease-in-out"
                       onClick={() => setOpen(true)}
                     >
                       G
                     </Button>
+                    <CustomToolTip
+                      content="Gallery"
+                      top="-30"
+                      left="-16"
+                      translateY="2"
+                    />
                   </Link>
-                  <Link href={`/genius-profile-2/${username}/projects`}>
+                  <Link
+                    href={`/genius-profile-2/${username}/projects`}
+                    className="group"
+                  >
                     <Button
                       className="rounded-full size-[32px] flex justify-center items-center bg-white text-black hover:bg-black hover:text-white transition-color duration-300 ease-in-out"
                       onClick={() => setOpen(true)}
                     >
                       P
                     </Button>
+                    <CustomToolTip
+                      content="Projects"
+                      top="-30"
+                      left="20"
+                      translateY="2"
+                    />
                   </Link>
                 </ul>
               </div>
@@ -85,7 +105,10 @@ export default function GeniusProfileLayout({
                       className={`sticky top-0 z-40 flex justify-center transition-all duration-300 ease-in-out`}
                     >
                       <ul className="relative mx-auto flex w-fit gap-1 rounded-full bg-white/20 p-1">
-                        <Link href={`/genius-profile-2/${username}/gallery`}>
+                        <Link
+                          href={`/genius-profile-2/${username}/gallery`}
+                          className="group"
+                        >
                           <Button
                             className={`rounded-full size-[32px] flex justify-center items-center transition-color duration-300 ease-in-out ${
                               pathname.includes("gallery")
@@ -95,8 +118,17 @@ export default function GeniusProfileLayout({
                           >
                             G
                           </Button>
+                          <CustomToolTip
+                            content="Gallery"
+                            top="40"
+                            left="-16"
+                            translateY="2"
+                          />
                         </Link>
-                        <Link href={`/genius-profile-2/${username}/projects`}>
+                        <Link
+                          href={`/genius-profile-2/${username}/projects`}
+                          className="group"
+                        >
                           <Button
                             className={`rounded-full size-[32px] flex justify-center items-center transition-color duration-300 ease-in-out ${
                               pathname.includes("projects")
@@ -106,6 +138,12 @@ export default function GeniusProfileLayout({
                           >
                             P
                           </Button>
+                          <CustomToolTip
+                            content="Projects"
+                            top="40"
+                            left="20"
+                            translateY="2"
+                          />
                         </Link>
                       </ul>
                     </div>
