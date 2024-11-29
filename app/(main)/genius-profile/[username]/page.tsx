ddd4157a482fdded4c8688a3c7d2abb7de37c05f
RohitSth <shrestha.rohit655@gmail.com>
2024-11-29 14:01:13 +0545

    update :: updated the profile

--- app/(main)/genius-profile/[username]/page.tsx
+++ app/(main)/genius-profile/[username]/page.tsx
@@ -1,3 +1,18 @@
-export default function GeniusProfile() {
-  return null;
+import GeniusUserHome from "@/components/comp/GeniusUserProfile/GeniusUserHome";
+
+interface GeniusUserInfoProps {
+  params: {
+    username: string;
+  };
+}
+
+export default async function GeniusUserProfile({
+  params,
+}: GeniusUserInfoProps) {
+  const { username } = params;
+  return (
+    <>
+      <GeniusUserHome username={username} />
+    </>
+  );
 }
