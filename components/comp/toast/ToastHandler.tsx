acdbae213b2f2e6b2a33aa0bca884791ceb96cf0
RohitSth <shrestha.rohit655@gmail.com>
2024-11-30 11:00:26 +0545

    update :: updated the top profile button

--- components/comp/toast/ToastHandler.tsx
+++ components/comp/toast/ToastHandler.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 const ToastHandler = () => {
   const searchParams = useSearchParams();
